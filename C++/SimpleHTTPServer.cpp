13b30248a3ab138019a4ce461914387f3e4be4b9
Trung-Rei <trantrung.trantrung.tt@gmail.com>
2020-07-02 21:33:27 +0700

    them class http server cu the (chua cai dat cac phuong thuc)

---
+++ C++/SimpleHTTPServer.cpp
@@ -0,0 +1,33 @@
+﻿#include "SimpleHTTPServer.h"
+#include <fstream>
+
+using namespace std;
+
+// chọn handler theo method
+std::vector<char> SimpleHTTPServer::select_method(HTTPRequestParser request)
+{
+    if (request.method == "GET")
+        return handle_GET(request);
+    else if (request.method == "POST")
+        return handle_POST(request);
+    else
+        return HTTP_501_handler(); // phương thức chưa được cài đặt xử lý
+}
+
+// xử lý GET request
+std::vector<char> SimpleHTTPServer::handle_GET(HTTPRequestParser request)
+{
+    return std::vector<char>();
+}
+
+// xử lý POST request
+std::vector<char> SimpleHTTPServer::handle_POST(HTTPRequestParser request)
+{
+    return std::vector<char>();
+}
+
+// trả về response 404
+std::vector<char> SimpleHTTPServer::HTTP_404_handler()
+{
+    return std::vector<char>();
+}
---
+++ C++/SimpleHTTPServer.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "BaseHTTPServer.h"
+
+/*
+
+Cài đặt http server cụ thể theo yêu cầu đồ án
+Bao gồm 2 phương thức GET và POST
+
+*/
+class SimpleHTTPServer :
+    public BaseHTTPServer
+{
+public:
+    SimpleHTTPServer(short port) : BaseHTTPServer(port) {}
+
+    std::vector<char> select_method(HTTPRequestParser request);
+
+    std::vector<char> handle_GET(HTTPRequestParser request);
+    std::vector<char> handle_POST(HTTPRequestParser request);
+
+    std::vector<char> HTTP_404_handler();
+};
+
